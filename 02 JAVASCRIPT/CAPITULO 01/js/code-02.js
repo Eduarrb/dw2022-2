4ec9f45be233ae030d8cf0143a41229830d5f468
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2022-05-09 21:35:23 -0500

    javascript 02 avance

--- 02 JAVASCRIPT/CAPITULO 01/js/code-02.js
+++ 02 JAVASCRIPT/CAPITULO 01/js/code-02.js
@@ -1,7 +1,30 @@
 /* 💥💥 NUMBERS 💥💥 */
-let num = 10;
-console.log(num);
-let num1 = '10';
-console.log(num1);
+// let num = 10;
+// console.log(num);
+// let num1 = '10';
+// console.log(num1);
 
 /* HOISTING */
+
+const pi = 3.1416;
+let radio = 11.5;
+console.log(pi, radio);
+
+// 💥💥 OPERACIONES MATEMATICAS
+let areaCirculo = pi * radio ** 2;
+console.log(areaCirculo);
+let suma = 10 + Number('30');
+let suma2 = 10 + parseInt('50');
+let suma3 = 10 + +'60';
+console.log(suma);
+console.log(suma2);
+console.log(suma3);
+
+// 💥💥 RESIDUO
+let num4 = 10;
+console.log(num4 % 2); // 0
+console.log(num4 % 3); // 1
+console.log(num4 % 4); // 2
+
+num4 = num4 + 1;
+console.log(num4);
