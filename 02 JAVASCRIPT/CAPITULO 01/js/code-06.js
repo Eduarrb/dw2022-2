40e13ab3d9212a362bc1118124b23cf1bb71fb6e
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2022-05-11 22:32:42 -0500

    javascript

---
+++ 02 JAVASCRIPT/CAPITULO 01/js/code-06.js
@@ -0,0 +1,36 @@
+/* ⚡⚡ FUNCIONES - FUNCTIONS ⚡⚡ */
+
+// saludar();
+
+var bloque = document.querySelector('.bloque1');
+// FUNCIONES DE EXPRESION
+function saludar(){
+    // PROCESOS DE EJECUCION AL SER LLAMADOS
+    console.log('hola a todos');
+    bloque.innerHTML = 'hola a todos';
+}
+saludar();
+
+const fechaNacimiento = 1990;
+
+function obtenerEdad(anioNacimiento){
+    // pienselo como una burbuja de ejecucion
+    // scope o ambito de bloques o ejecuciones
+    let edad = 2022 - anioNacimiento;
+    console.log(edad);
+}
+
+obtenerEdad(fechaNacimiento);
+
+function suma(a, b){
+    let res = a + b;
+    // console.log(res);
+    // console.log('palabra');
+    return res;
+}
+suma(2, 6);
+// flotando el return
+// let respuesta = suma(2,6.2);
+// console.log(respuesta);
+
+console.log(`el resultado es ${suma(2,3)}`);
