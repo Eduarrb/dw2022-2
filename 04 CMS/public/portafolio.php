3f6ff0ec237d656b65e0d1a227a374c77ea375b4
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2022-06-20 21:33:44 -0500

    portafiolo plantilla final

--- 04 CMS/public/portafolio.php
+++ 04 CMS/public/portafolio.php
@@ -4,7 +4,21 @@
 
 <?php include(VIEW_FRONT . DS . "header_port.php"); ?>
 
-
+    <section class="portafolio pt-10">
+        <div class="portafolio__contenedor contenedor">
+            <img src="img/portafolio/01-thumbnail.jpg" alt="" class="block-center">
+            <h2 class="text-center titulo-n2 mt-3">titulo</h2>
+            <p class="descri-n2 text-center">descri</p>
+            <p class="descri-n3 text-center">
+                <i class="fa-solid fa-eye"></i> 12
+            </p>
+            <div class="d-flex justify-evenly mt-5">
+                <div class="descri-n3"><strong>Por Eduardo Arroyo</strong></div>
+                <div class="descri-n3">2022-06-20</div>
+            </div>
+            <p class="descri-n3 mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi libero odit rerum repudiandae fugit necessitatibus, deleniti iusto pariatur eaque ut facilis ex, ipsum quo dignissimos expedita incidunt eius modi distinctio inventore corporis odio iure! Est quod minima magnam ad odit unde commodi animi nobis minus hic libero, a odio debitis fugit non porro velit. Harum officia consectetur minima ullam iusto recusandae hic, corrupti ut consequatur vel facilis officiis aliquid sit odit possimus nobis at laudantium, ducimus deleniti iste animi error eligendi sequi! Delectus facilis repellendus nihil, repudiandae soluta fuga quae quas aliquam ut esse omnis consequatur iste vitae, velit nobis.</p>
+        </div>
+    </section>
 
 
 
